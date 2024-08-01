68ab749edfcea3291ded080bbb57302c73e50877
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-01 04:14:05 +0200

    ingredients object

---
+++ src/js/foods/ingredients.mjs
@@ -0,0 +1,142 @@
+export const ingredients = {
+  proteins: [
+    {
+      name: 'egg',
+      price: {
+        perKg: 49.71,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 149,
+        protein: 13,
+        fats: 11,
+        carbohydrates: 0.3,
+      },
+      gramPiece: 56.68,
+      sizePiece: 'medium',
+    },
+    {
+      name: 'fresh salmon filet',
+      price: {
+        perKg: 279.6,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 254,
+        protein: 21.1,
+        fats: 17,
+        carbohydrates: 4.3,
+      },
+    },
+    {
+      name: 'chicken',
+      price: {
+        perKg: 179.43,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 112,
+        protein: 23,
+        fats: 2.1,
+        carbohydrates: 0,
+      },
+    },
+  ],
+  nuts: [
+    {
+      name: 'almond',
+      price: {
+        perKg: 300,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 595,
+        protein: 21,
+        fats: 52,
+        carbohydrates: 21,
+      },
+      gramPiece: 1,
+      sizePiece: 'normal',
+    },
+  ],
+  vegetables: [
+    {
+      name: 'frozen broccoli',
+      price: {
+        perKg: 44.89,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 30,
+        protein: 3.2,
+        fats: 0.5,
+        carbohydrates: 2.1,
+      },
+    },
+    {
+      name: 'frozen brussels sprouts',
+      price: {
+        perKg: 48.5,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 43,
+        protein: 3.8,
+        fats: 0.5,
+        carbohydrates: 4.3,
+      },
+    },
+    {
+      name: 'frozen asparagus beans',
+      price: {
+        perKg: 76.67,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 28,
+        protein: 1.7,
+        fats: 0.5,
+        carbohydrates: 3,
+      },
+    },
+    {
+      name: 'fresh ripe avocado',
+      price: {
+        perKg: 93.44,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 191,
+        protein: 1.8,
+        fats: 19.6,
+        carbohydrates: 0.4,
+      },
+      gramPiece: 160,
+      sizePiece: 'medium',
+    },
+    {
+      name: 'sweet potato',
+      price: {
+        perKg: 50,
+        currency: 'nok',
+      },
+      macros: {
+        grams: 100,
+        kcal: 85,
+        protein: 1.6,
+        fats: 0.1,
+        carbohydrates: 20,
+      },
+      gramPiece: 300,
+      sizePiece: 'medium',
+    },
+  ],
+};
