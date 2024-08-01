846d98df68c580d309c2e5f0d8a17c037a1db803
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-01 21:59:35 +0200

    function to portion chicken

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -3,6 +3,7 @@ import { ingredients } from './foods/ingredients.mjs';
 import { salad } from './foods/salad.mjs';
 import { omeletteSideAlmonds } from './meals/breakfast/omeletteSideAlmonds.mjs';
 import { salmonFiletSideAlmonds } from './meals/breakfast/salmonFiletSideAlmonds.mjs';
+import { portionChicken } from './meals/prep/portionChicken.mjs';
 
 console.log('foods:', ingredients);
 console.log('Salad Ingredients:', salad);
@@ -15,3 +16,5 @@ mixSalad();
 console.log('Breakfasts:');
 omeletteSideAlmonds();
 salmonFiletSideAlmonds();
+
+portionChicken(300);
