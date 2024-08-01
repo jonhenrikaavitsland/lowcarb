381af31c78eeef1df50f191c778f85a0bdaa7fff
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-01 06:44:42 +0200

    salad object

--- src/js/data/mixSalad.mjs
+++ src/js/data/mixSalad.mjs
@@ -6,12 +6,42 @@ import { pricePerIngredient } from './pricePerIngredient.mjs';
 export function mixSalad() {
   const mixedSaladIngredients = [];
 
+  const totalMacros = {
+    kcal: 0,
+    protein: 0,
+    fats: 0,
+    carbohydrates: 0,
+  };
+
+  let priceTotal = 0;
+
   salad.forEach((ingredient) => {
     const pricePer = pricePerIngredient(ingredient);
     const macrosPerPiece = macrosPerIngredient(ingredient);
     const newIngredient = newIngredientObject(ingredient, pricePer, macrosPerPiece);
 
+    totalMacros.kcal += macrosPerPiece.kcal;
+    totalMacros.protein += macrosPerPiece.protein;
+    totalMacros.fats += macrosPerPiece.fats;
+    totalMacros.carbohydrates += macrosPerPiece.carbohydrates;
+
+    priceTotal += pricePer;
+
     mixedSaladIngredients.push(newIngredient);
   });
-  console.log(mixedSaladIngredients);
+
+  const finalSalad = {
+    name: 'Mixed Salad',
+    servings: 4,
+    totalPrice: parseFloat(priceTotal.toFixed(2)),
+    totalMacros: {
+      kcal: parseFloat(totalMacros.kcal.toFixed(2)),
+      protein: parseFloat(totalMacros.protein.toFixed(2)),
+      fats: parseFloat(totalMacros.fats.toFixed(2)),
+      carbohydrates: parseFloat(totalMacros.carbohydrates.toFixed(2)),
+    },
+    ingredients: mixedSaladIngredients,
+  };
+  console.log('Mixed Salad:', finalSalad);
+  return finalSalad;
 }
