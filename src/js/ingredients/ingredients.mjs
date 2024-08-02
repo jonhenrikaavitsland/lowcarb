fe27276c190af76b24f461f1345522f3bade61a6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-02 04:57:40 +0200

    ingredients refactored into a single object with same structure

---
+++ src/js/ingredients/ingredients.mjs
@@ -0,0 +1,332 @@
+export const ingredients = {
+  proteins: [
+    {
+      name: 'egg',
+      price: {
+        perKg: 49.71,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 149,
+        protein: 13,
+        fats: 11,
+        carbohydrates: 0.3,
+      },
+      portionSizes: [56.68],
+    },
+    {
+      name: 'salmon',
+      price: {
+        perKg: 279.6,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 254,
+        protein: 21.1,
+        fats: 17,
+        carbohydrates: 4.3,
+      },
+      portionSizes: [250, 260, 300],
+    },
+    {
+      name: 'chicken',
+      price: {
+        perKg: 179.43,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 112,
+        protein: 23,
+        fats: 2.1,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'barbecue ribs',
+      price: {
+        perKg: 118.63,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 382,
+        protein: 14,
+        fats: 36,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'neck chops',
+      price: {
+        perKg: 83.9,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 206,
+        protein: 18.3,
+        fats: 14.8,
+        carbohydrates: 0.1,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'pork chops',
+      price: {
+        perKg: 87.28,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 244,
+        protein: 20,
+        fats: 18,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'beef pepper steak',
+      price: {
+        perKg: 372.82,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 111,
+        protein: 22,
+        fats: 2.3,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'entrecote',
+      price: {
+        perKg: 427.6,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 186,
+        protein: 20.6,
+        fats: 11.5,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+    {
+      name: 'pollock',
+      price: {
+        perKg: 231.5,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 69,
+        protein: 16.5,
+        fats: 0.3,
+        carbohydrates: 0,
+      },
+      portionSizes: [250, 300],
+    },
+  ],
+  nuts: [
+    {
+      name: 'almond',
+      price: {
+        perKg: 300,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 595,
+        protein: 21,
+        fats: 52,
+        carbohydrates: 21,
+      },
+      portionSizes: [20],
+    },
+  ],
+  vegetables: [
+    {
+      name: 'broccoli',
+      price: {
+        perKg: 44.89,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 30,
+        protein: 3.2,
+        fats: 0.5,
+        carbohydrates: 2.1,
+      },
+      portionSizes: [150],
+    },
+    {
+      name: 'brussels sprouts',
+      price: {
+        perKg: 48.5,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 43,
+        protein: 3.8,
+        fats: 0.5,
+        carbohydrates: 4.3,
+      },
+      portionSizes: [150],
+    },
+    {
+      name: 'asparagus beans',
+      price: {
+        perKg: 76.67,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 28,
+        protein: 1.7,
+        fats: 0.5,
+        carbohydrates: 3,
+      },
+      portionSizes: [150],
+    },
+    {
+      name: 'fresh ripe avocado',
+      price: {
+        perKg: 93.44,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 191,
+        protein: 1.8,
+        fats: 19.6,
+        carbohydrates: 0.4,
+      },
+      portionSizes: [160],
+    },
+    {
+      name: 'sweet potato',
+      price: {
+        perKg: 50,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 85,
+        protein: 1.6,
+        fats: 0.1,
+        carbohydrates: 20,
+      },
+      portionSizes: [300],
+    },
+  ],
+  salad: [
+    {
+      name: 'crispi',
+      price: {
+        perKg: 210.56,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 12,
+        protein: 0.8,
+        fats: 0.1,
+        carbohydrates: 1.5,
+      },
+      portionSizes: [45],
+    },
+    {
+      name: 'red cherry tomato',
+      price: {
+        perKg: 104,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 18,
+        protein: 0.7,
+        fats: 0.2,
+        carbohydrates: 2.5,
+      },
+      portionSizes: [31.25],
+    },
+    {
+      name: 'cucumber',
+      price: {
+        perKg: 60,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 15,
+        protein: 0.6,
+        fats: 0,
+        carbohydrates: 3.6,
+      },
+      portionSizes: [125],
+    },
+    {
+      name: 'red grapes',
+      price: {
+        perKg: 79.8,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 62,
+        protein: 0.7,
+        fats: 0.2,
+        carbohydrates: 13.8,
+      },
+      portionSizes: [62.5],
+    },
+    {
+      name: 'red onion',
+      price: {
+        perKg: 76,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 39,
+        protein: 1.1,
+        fats: 0.1,
+        carbohydrates: 9,
+      },
+      portionSizes: [47],
+    },
+    {
+      name: 'yellow or red paprika',
+      price: {
+        perKg: 80,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 26,
+        protein: 0.8,
+        fats: 0.2,
+        carbohydrates: 4.2,
+      },
+      portionSizes: [100],
+    },
+    {
+      name: 'beetroot',
+      price: {
+        perKg: 93.8,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 49,
+        protein: 1.6,
+        fats: 0.2,
+        carbohydrates: 8.9,
+      },
+      portionSizes: [62.5],
+    },
+    {
+      name: 'celeriac',
+      price: {
+        perKg: 70,
+        currency: 'nok',
+      },
+      macros: {
+        kcal: 16,
+        protein: 1.1,
+        fats: 0.1,
+        carbohydrates: 1.3,
+      },
+      portionSizes: [12.5],
+    },
+  ],
+};
