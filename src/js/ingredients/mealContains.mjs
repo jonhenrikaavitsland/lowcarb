801ddd419f485008e9693dae074018a4f6bef179
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-02 05:29:27 +0200

    created a object with meal receipes

---
+++ src/js/ingredients/mealContains.mjs
@@ -0,0 +1,286 @@
+export const mealContains = [
+  {
+    name: 'omelette w/ almonds on the side',
+    category: 'breakfast',
+    contains: [
+      {
+        ingredient: 'egg',
+        weight: 260,
+      },
+      {
+        ingredient: 'almond',
+        weight: 20,
+      },
+    ],
+  },
+  {
+    name: 'salmon filet w/ almonds on the side',
+    category: 'breakfast',
+    contains: [
+      {
+        ingredient: 'salmon',
+        weight: 260,
+      },
+      {
+        ingredient: 'almond',
+        weight: 20,
+      },
+    ],
+  },
+  {
+    name: 'mixed salad',
+    category: 'side',
+    contains: [
+      {
+        ingredient: 'crispi',
+        weight: 45,
+      },
+      {
+        ingredient: 'red cherry tomato',
+        weight: 31.25,
+      },
+      {
+        ingredient: 'cucumber',
+        weight: 31.25,
+      },
+      {
+        ingredient: 'red grapes',
+        weight: 62.5,
+      },
+      {
+        ingredient: 'red onion',
+        weight: 47,
+      },
+      {
+        ingredient: 'yellow or red paprika',
+        weight: 100,
+      },
+      {
+        ingredient: 'beetroot',
+        weight: 62.5,
+      },
+      {
+        ingredient: 'celeriac',
+        weight: 12.5,
+      },
+    ],
+  },
+  {
+    name: 'omelette w/ broccoli, avocado, salad on the side',
+    category: 'lunch',
+    contains: [
+      {
+        ingredient: 'egg',
+        weight: 300,
+      },
+      {
+        ingredient: 'avocado',
+        weight: 160,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+      {
+        ingredient: 'mixed salad',
+        weight: 392,
+      },
+    ],
+  },
+  {
+    name: 'salmon w/ asparagus beans, avocado, salad on the side',
+    category: 'lunch',
+    contains: [
+      {
+        ingredient: 'salmon',
+        weight: 300,
+      },
+      {
+        ingredient: 'avocado',
+        weight: 160,
+      },
+      {
+        ingredient: 'asparagus beans',
+        weight: 150,
+      },
+      {
+        ingredient: 'mixed salad',
+        weight: 392,
+      },
+    ],
+  },
+  {
+    name: 'chicken w/ asparagus beans, avocado, salad on the side',
+    category: 'lunch',
+    contains: [
+      {
+        ingredient: 'chicken',
+        weight: 300,
+      },
+      {
+        ingredient: 'avocado',
+        weight: 160,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+      {
+        ingredient: 'mixed salad',
+        weight: 392,
+      },
+    ],
+  },
+  {
+    name: 'neck chops w/ broccoli, avocado, salad on the side',
+    category: 'lunch',
+    contains: [
+      {
+        ingredient: 'neck chops',
+        weight: 300,
+      },
+      {
+        ingredient: 'avocado',
+        weight: 160,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+      {
+        ingredient: 'mixed salad',
+        weight: 392,
+      },
+    ],
+  },
+  {
+    name: 'pollock w/ asparagus beans, avocado, salad on the side',
+    category: 'lunch',
+    contains: [
+      {
+        ingredient: 'pollock',
+        weight: 300,
+      },
+      {
+        ingredient: 'avocado',
+        weight: 160,
+      },
+      {
+        ingredient: 'asparagus beans',
+        weight: 150,
+      },
+      {
+        ingredient: 'mixed salad',
+        weight: 392,
+      },
+    ],
+  },
+  {
+    name: 'salmon w/ broccoli, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'salmon',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+    ],
+  },
+  {
+    name: 'chicken w/ asparagus beans, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'chicken',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'asparagus beans',
+        weight: 150,
+      },
+    ],
+  },
+  {
+    name: 'barbecue ribs w/ broccoli, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'barbecue ribs',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+    ],
+  },
+  {
+    name: 'pork chops w/ brussels sprouts, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'pork chops',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'brussels sprouts',
+        weight: 150,
+      },
+    ],
+  },
+  {
+    name: 'beef pepper steak w/ broccoli, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'beef pepper steak',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'broccoli',
+        weight: 150,
+      },
+    ],
+  },
+  {
+    name: 'entrecote w/ brussels sprouts, sweet potato on the side',
+    category: 'dinner',
+    contains: [
+      {
+        ingredient: 'entrecote',
+        weight: 250,
+      },
+      {
+        ingredient: 'sweet potato',
+        weight: 300,
+      },
+      {
+        ingredient: 'brussels sprouts',
+        weight: 150,
+      },
+    ],
+  },
+];
